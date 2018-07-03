97a1167226e8362181dec357337a28b07e68fa7a
amits121 <amit@circarconsulting.com>
2018-07-02 20:48:41 -0700

    code modified

--- p_writing.py
+++ p_writing.py
@@ -2,5 +2,3 @@ filename = 'pythonclass.txt'
 with open(filename, 'w') as file_content:
     file_content.write("This python class have four students. ")
     file_content.write("Their names are; Seema, Priyanka, Ajay and Gaurav.")
-with open(filename, 'a') as file_content:	
-    file_content.write("They arelearning fast") 
